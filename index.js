9542ef3ee5b73ba9a5d6ad0a19919dc721559666
Andriana Kovalyshyn <an.kvellia@gmail.com>
2019-02-12 10:03:12 +0200

    added image stream

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const dir = path.join(__dirname, 'app/data');
+
+
 const PORT = 3050
 
 const { eventList, details } = require('./app/data')
 const { joinById } = require('./app/join-by-id')
 
 const requestHandler = (req, res) => {
-  const data = joinById(eventList, details)
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
+  const file = path.join(dir, 'jokulsarlon_ice.jpg')
+  
+  const s = fs.createReadStream(file)
+  s.on('open', function () {
+      res.setHeader('Content-Type', 'image/jpeg')
+      s.pipe(res)
   })
-  res.end(JSON.stringify({ events: data }))
+  s.on('error', function () {
+      res.setHeader('Content-Type', 'text/plain')
+      res.statusCode = 404
+      res.end('Not found')
+  });
 }
 
 const server = http.createServer(requestHandler)
-
 server.listen(PORT, (err) => {
   if (err) {
     return console.log('The connection was terminated due to ', err)
